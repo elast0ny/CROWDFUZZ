c4645e00281128933a997a045f9eb5fb66b5a5b5
ElasT0ny <elast0ny00@gmail.com>
2020-10-30 10:44:53 -0400

    respect no_mutate store key

--- plugins/basic_mutate/src/basic_mutate.rs
+++ plugins/basic_mutate/src/basic_mutate.rs
@@ -13,22 +13,26 @@ cflib::register!(unload, destroy);
 struct State {
     /// fast/non-crypto grade random
     rng: SmallRng,
+    no_mutate: &'static bool,
     /// Reference to the currently selected input
     cur_input: &'static mut CfInput,
+    
 }
 
 // Initialize our plugin
-fn init(_core: &mut dyn PluginInterface, _store: &mut CfStore) -> Result<*mut u8> {
+fn init(core: &mut dyn PluginInterface, store: &mut CfStore) -> Result<*mut u8> {
     #[allow(invalid_value)]
-    let state = Box::new(unsafe {
+    let s = Box::new(unsafe {
         State {
             rng: SmallRng::from_rng(&mut ::rand::thread_rng()).unwrap(),
+            // core store values
+            no_mutate: store.as_mutref(STORE_NO_MUTATE, Some(core))?,
             // Plugin store values
             cur_input: MaybeUninit::zeroed().assume_init(),
         }
     });
 
-    Ok(Box::into_raw(state) as _)
+    Ok(Box::into_raw(s) as _)
 }
 
 // Make sure we have everything to fuzz properly
@@ -37,11 +41,11 @@ fn validate(
     store: &mut CfStore,
     plugin_ctx: *mut u8,
 ) -> Result<()> {
-    let state = box_ref!(plugin_ctx, State);
+    let s = box_ref!(plugin_ctx, State);
 
     // We need a plugin that creates in input_bytes
     unsafe {
-        state.cur_input = store.as_mutref(STORE_INPUT_BYTES, Some(core))?;
+        s.cur_input = store.as_mutref(STORE_INPUT_BYTES, Some(core))?;
     }
 
     Ok(())
@@ -53,19 +57,19 @@ fn mutate_input(
     _store: &mut CfStore,
     plugin_ctx: *mut u8,
 ) -> Result<()> {
-    let state = box_ref!(plugin_ctx, State);
+    let s = box_ref!(plugin_ctx, State);
 
-    if state.cur_input.chunks.is_empty() {
+    if *s.no_mutate || s.cur_input.chunks.is_empty() {
         // Input is empty ??
         return Ok(());
     }
 
-    let first_chunk = unsafe { state.cur_input.chunks.get_unchecked_mut(0) };
+    let first_chunk = unsafe { s.cur_input.chunks.get_unchecked_mut(0) };
 
     // Randomly mutate some bytes in the first chunk
-    let num_of_bytes_mutated = state.rng.gen_range(0, first_chunk.len());
+    let num_of_bytes_mutated = s.rng.gen_range(0, first_chunk.len());
     for _ in 0..num_of_bytes_mutated {
-        first_chunk[state.rng.gen_range(0, first_chunk.len())] = state.rng.gen::<u8>();
+        first_chunk[s.rng.gen_range(0, first_chunk.len())] = s.rng.gen::<u8>();
     }
 
     Ok(())
